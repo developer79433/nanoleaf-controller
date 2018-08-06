02d9f5e6e9ea5ab1e0bbd604d7f38db320f3e1b0
Some Guy <cnhfci3wngoifuxq3nbkuxqn3guykfgxwunwfk4@mailinator.com>
2018-08-06 13:31:33 +1000

    Use curl instead of curlpp. My installation of curlpp seems broken.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <cstdlib>
-#include <curlpp/cURLpp.hpp>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+#define URL "http://192.168.1.17/"
 
 int
 main(int argc, char *argv[])
 {
-	curlpp::initialize();
-	curlpp::terminate();
+	CURL *curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, URL);
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+	CURLcode res = curl_easy_perform(curl);
+	if (res != CURLE_OK) {
+		fprintf(
+			stderr,
+			"curl_easy_perform() failed: %s\n",
+			curl_easy_strerror(res)
+		);
+	}
+	curl_easy_cleanup(curl);
 	return EXIT_SUCCESS;
 }
