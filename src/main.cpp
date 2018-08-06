98e6dc9c22fad6bf2dcaf1226e0727a89f95bc95
Some Guy <cnhfci3wngoifuxq3nbkuxqn3guykfgxwunwfk4@mailinator.com>
2018-08-06 15:44:14 +1000

    Connect to the HTTP interface on the discovered lights.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
 #include <curl/curl.h>
 #include <curl/easy.h>
@@ -10,6 +11,76 @@
 
 #define NANOLEAF_MDNS_SERVICE_TYPE "_nanoleafapi._tcp"
 #define URL "http://192.168.1.17/"
+
+void
+make_url(std::ostringstream &url, const char *host_name, unsigned short port, bool use_ssl = false)
+{
+	url << "http" << (use_ssl ? "s" : "") << "://" << host_name <<
+			":" << port << "/";
+}
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+	std::cerr.write(ptr, size * nmemb);
+	return size * nmemb;
+}
+
+void
+do_connect_to_panels(CURL *curl, const char *host_name, unsigned short port)
+{
+	std::ostringstream url;
+	make_url(url, host_name, port);
+	std::cerr << "Connecting to '" << url.str().c_str() << "'" << std::endl;
+	CURLcode res;
+	res = curl_easy_setopt(curl, CURLOPT_URL, url.str().c_str());
+	if (res != CURLE_OK) {
+		throw curl_easy_strerror(res);
+	}
+	res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
+	if (res != CURLE_OK) {
+		throw curl_easy_strerror(res);
+	}
+	res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	if (res != CURLE_OK) {
+		throw curl_easy_strerror(res);
+	}
+	res = curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+	if (res != CURLE_OK) {
+		throw curl_easy_strerror(res);
+	}
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK) {
+		throw curl_easy_strerror(res);
+	}
+	if (res != CURLE_OK) {
+		throw curl_easy_strerror(res);
+	}
+}
+
+class Curl {
+private:
+	CURL *curl;
+public:
+	Curl(void) : curl(curl_easy_init()) {}
+	virtual ~Curl() {
+		if (curl) {
+			curl_easy_cleanup(curl);
+		}
+	}
+	operator CURL*(void) { return curl; }
+	operator const CURL*(void) { return curl; }
+};
+
+void
+connect_to_panels(const char *host_name, unsigned short port)
+{
+	Curl curl;
+	do_connect_to_panels(curl, host_name, port);
+}
+
+struct resolve_callback_args {
+	AvahiClient *client;
+};
 
 static AvahiSimplePoll *simple_poll = NULL;
 static void resolve_callback(
@@ -25,8 +96,10 @@ static void resolve_callback(
     uint16_t port,
     AvahiStringList *txt,
     AvahiLookupResultFlags flags,
-    AVAHI_GCC_UNUSED void* userdata) {
+    void* userdata) {
     assert(r);
+    resolve_callback_args *args = static_cast<resolve_callback_args *>(userdata);
+    (void) args;
     /* Called whenever a service has been resolved successfully or timed out */
     switch (event) {
         case AVAHI_RESOLVER_FAILURE:
@@ -40,17 +113,22 @@ static void resolve_callback(
                     "\t" << host_name << ":" << port << " (" << a << ")" << std::endl <<
                     "\tTXT=" << t << std::endl <<
                     "\tcookie is " << avahi_string_list_get_service_cookie(txt) << std::endl <<
-                    "\tis_local: %i" << !!(flags & AVAHI_LOOKUP_RESULT_LOCAL) << std::endl <<
-                    "\tour_own: %i" << !!(flags & AVAHI_LOOKUP_RESULT_OUR_OWN) << std::endl <<
-                    "\twide_area: %i" << !!(flags & AVAHI_LOOKUP_RESULT_WIDE_AREA) << std::endl <<
-                    "\tmulticast: %i" << !!(flags & AVAHI_LOOKUP_RESULT_MULTICAST) << std::endl <<
-                    "\tcached: %i" << !!(flags & AVAHI_LOOKUP_RESULT_CACHED) << std::endl
+                    "\tis_local: " << !!(flags & AVAHI_LOOKUP_RESULT_LOCAL) << std::endl <<
+                    "\tour_own: " << !!(flags & AVAHI_LOOKUP_RESULT_OUR_OWN) << std::endl <<
+                    "\twide_area: " << !!(flags & AVAHI_LOOKUP_RESULT_WIDE_AREA) << std::endl <<
+                    "\tmulticast: " << !!(flags & AVAHI_LOOKUP_RESULT_MULTICAST) << std::endl <<
+                    "\tcached: " << !!(flags & AVAHI_LOOKUP_RESULT_CACHED) << std::endl
 			;
+            connect_to_panels(host_name, port);
             avahi_free(t);
         }
     }
     avahi_service_resolver_free(r);
 }
+
+struct browse_callback_args {
+	AvahiClient *client;
+};
 
 static void browse_callback(
     AvahiServiceBrowser *b,
@@ -62,7 +140,7 @@ static void browse_callback(
     const char *domain,
     AVAHI_GCC_UNUSED AvahiLookupResultFlags flags,
     void* userdata) {
-    AvahiClient *c = static_cast<AvahiClient *>(userdata);
+	browse_callback_args *args = static_cast<browse_callback_args *>(userdata);
     assert(b);
     /* Called whenever a new services becomes available on the LAN or is removed from the LAN */
     const char *errmsg;
@@ -72,13 +150,15 @@ static void browse_callback(
             avahi_simple_poll_quit(simple_poll);
             throw errmsg;
         case AVAHI_BROWSER_NEW:
-            fprintf(stderr, "(Browser) NEW: service '%s' of type '%s' in domain '%s'\n", name, type, domain);
+            std::cerr << "(Browser) NEW: service '" << name << "' of type '" << type << "' in domain '" << domain << "'" << std::endl;
             /* We ignore the returned resolver object. In the callback
                function we free it. If the server is terminated before
                the callback function is called the server will free
                the resolver for us. */
-            if (!(avahi_service_resolver_new(c, interface, protocol, name, type, domain, AVAHI_PROTO_UNSPEC, AVAHI_LOOKUP_USE_MULTICAST, resolve_callback, c)))
-                throw avahi_strerror(avahi_client_errno(c));
+            struct resolve_callback_args resolve_args;
+            resolve_args.client = args->client;
+            if (!(avahi_service_resolver_new(args->client, interface, protocol, name, type, domain, AVAHI_PROTO_UNSPEC, AVAHI_LOOKUP_USE_MULTICAST, resolve_callback, &resolve_args)))
+                throw avahi_strerror(avahi_client_errno(args->client));
             break;
         case AVAHI_BROWSER_REMOVE:
 #ifndef NDEBUG
@@ -93,7 +173,14 @@ static void browse_callback(
             break;
     }
 }
-static void client_callback(AvahiClient *c, AvahiClientState state, AVAHI_GCC_UNUSED void * userdata) {
+
+struct client_callback_args {
+	AvahiClient *client;
+};
+
+static void client_callback(AvahiClient *c, AvahiClientState state, AVAHI_GCC_UNUSED void *userdata) {
+	client_callback_args *args = static_cast<client_callback_args *>(userdata);
+	(void) args;
     assert(c);
     /* Called whenever the client or server state changes */
     if (state == AVAHI_CLIENT_FAILURE) {
@@ -112,23 +199,26 @@ discover(void)
         throw avahi_strerror(error);
     }
     const AvahiPoll *poll = avahi_simple_poll_get(simple_poll);
-    AvahiClient *client = avahi_client_new(poll, AVAHI_CLIENT_NO_FAIL, client_callback, NULL, &error);
-    if (!client) {
+    struct client_callback_args client_args;
+    client_args.client = avahi_client_new(poll, AVAHI_CLIENT_NO_FAIL, client_callback, &client_args, &error);
+    if (!client_args.client) {
         throw avahi_strerror(error);
     }
     /* Create the service browser */
+    browse_callback_args browse_args;
+    browse_args.client = client_args.client;
     AvahiServiceBrowser *sb = avahi_service_browser_new(
-    	client,
+    	client_args.client,
 		AVAHI_IF_UNSPEC,
 		AVAHI_PROTO_UNSPEC,
 		NANOLEAF_MDNS_SERVICE_TYPE,
 		NULL,
 		AVAHI_LOOKUP_USE_MULTICAST,
 		browse_callback,
-		client
+		&browse_args
 	);
     if (!sb) {
-        throw avahi_strerror(avahi_client_errno(client));
+        throw avahi_strerror(avahi_client_errno(client_args.client));
     }
     /* Run the main loop */
     avahi_simple_poll_loop(simple_poll);
@@ -137,18 +227,11 @@ discover(void)
 int
 main(int argc, char *argv[])
 {
-	discover();
-	CURL *curl = curl_easy_init();
-	curl_easy_setopt(curl, CURLOPT_URL, URL);
-	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-	CURLcode res = curl_easy_perform(curl);
+	CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
 	if (res != CURLE_OK) {
-		fprintf(
-			stderr,
-			"curl_easy_perform() failed: %s\n",
-			curl_easy_strerror(res)
-		);
+		throw curl_easy_strerror(res);
 	}
-	curl_easy_cleanup(curl);
+	discover();
+	curl_global_cleanup();
 	return EXIT_SUCCESS;
 }
