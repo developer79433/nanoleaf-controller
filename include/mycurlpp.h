5c6582084c1f2b4ea711d381526e3718e5ac6ab0
Some Guy <cnhfci3wngoifuxq3nbkuxqn3guykfgxwunwfk4@mailinator.com>
2018-08-06 18:20:23 +1000

    Move the Curl wrapper into its own header file.

---
+++ include/mycurlpp.h
@@ -0,0 +1,90 @@
+#ifndef MYCURLPP_H
+#define MYCURLPP_H 1
+
+#include <sstream>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+namespace mycurlpp {
+
+class Curl {
+private:
+	CURL *curl;
+	std::string hostname;
+	unsigned int port;
+	bool use_ssl;
+	std::string path;
+	long last_response_code;
+public:
+	Curl(
+		const char *phostname = NULL,
+		unsigned int pport = 80,
+		bool puse_ssl = false,
+		const char *ppath = "/"
+	) :
+		curl(curl_easy_init()),
+		hostname(phostname),
+		port(pport),
+		use_ssl(puse_ssl),
+		path(ppath)
+	{
+	}
+	virtual ~Curl() {
+		if (curl) {
+			curl_easy_cleanup(curl);
+		}
+	}
+	void set_hostname(std::string &new_hostname) {
+		hostname = new_hostname;
+	}
+	void set_port(unsigned short new_port) {
+		port = new_port;
+	}
+	void set_ssl(bool new_ssl) {
+		use_ssl = new_ssl;
+	}
+	void set_path(const std::string &new_path) {
+		path = new_path;
+	}
+	void make_url(
+		std::ostringstream &url
+	) {
+		url.clear();
+		url << "http" << (use_ssl ? "s" : "") << "://" << hostname <<
+				":" << port << path;
+	}
+	std::string get_url(void) {
+		std::ostringstream url;
+		make_url(url);
+		return url.str();
+	}
+	operator CURL*(void) { return curl; }
+	operator const CURL*(void) { return curl; }
+	template<typename T> void setopt(CURLoption opt, const T& val) {
+		CURLcode res;
+		res = curl_easy_setopt(curl, opt, val);
+		if (res != CURLE_OK) {
+			throw curl_easy_strerror(res);
+		}
+	}
+	unsigned int get_status(void) const { return static_cast<unsigned int>(last_response_code); }
+	unsigned int perform(void) {
+		std::ostringstream surl;
+		make_url(surl);
+		const std::string &url = surl.str();
+		std::cerr << "Connecting to '" << url << "'" << std::endl;
+		setopt(CURLOPT_URL, url.c_str());
+		CURLcode res;
+		res = curl_easy_perform(curl);
+		if (res == CURLE_OK) {
+			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &last_response_code);
+		} else {
+			throw curl_easy_strerror(res);
+		}
+		return get_status();
+	}
+};
+
+}
+
+#endif /* MYCURLPP_H */
--- src/main.cpp
+++ src/main.cpp
@@ -13,87 +13,11 @@
 #include <avahi-common/error.h>
 #include <nlohmann/json.hpp>
 
+#include "mycurlpp.h"
+
 using json = nlohmann::json;
 
 #define NANOLEAF_MDNS_SERVICE_TYPE "_nanoleafapi._tcp"
-
-class Curl {
-private:
-	CURL *curl;
-	std::string hostname;
-	unsigned int port;
-	bool use_ssl;
-	std::string path;
-	long last_response_code;
-public:
-	Curl(
-		const char *phostname = NULL,
-		unsigned int pport = 80,
-		bool puse_ssl = false,
-		const char *ppath = "/"
-	) :
-		curl(curl_easy_init()),
-		hostname(phostname),
-		port(pport),
-		use_ssl(puse_ssl),
-		path(ppath)
-	{
-	}
-	virtual ~Curl() {
-		if (curl) {
-			curl_easy_cleanup(curl);
-		}
-	}
-	void set_hostname(std::string &new_hostname) {
-		hostname = new_hostname;
-	}
-	void set_port(unsigned short new_port) {
-		port = new_port;
-	}
-	void set_ssl(bool new_ssl) {
-		use_ssl = new_ssl;
-	}
-	void set_path(const std::string &new_path) {
-		path = new_path;
-	}
-	void make_url(
-		std::ostringstream &url
-	) {
-		url.clear();
-		url << "http" << (use_ssl ? "s" : "") << "://" << hostname <<
-				":" << port << path;
-	}
-	std::string get_url(void) {
-		std::ostringstream url;
-		make_url(url);
-		return url.str();
-	}
-	operator CURL*(void) { return curl; }
-	operator const CURL*(void) { return curl; }
-	template<typename T> void setopt(CURLoption opt, const T& val) {
-		CURLcode res;
-		res = curl_easy_setopt(curl, opt, val);
-		if (res != CURLE_OK) {
-			throw curl_easy_strerror(res);
-		}
-	}
-	unsigned int get_status(void) const { return static_cast<unsigned int>(last_response_code); }
-	unsigned int perform(void) {
-		std::ostringstream surl;
-		make_url(surl);
-		const std::string &url = surl.str();
-		std::cerr << "Connecting to '" << url << "'" << std::endl;
-		setopt(CURLOPT_URL, url.c_str());
-		CURLcode res;
-		res = curl_easy_perform(curl);
-		if (res == CURLE_OK) {
-			curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &last_response_code);
-		} else {
-			throw curl_easy_strerror(res);
-		}
-		return get_status();
-	}
-};
 
 #define TOKEN_FILENAME "auth_token.dat"
 
@@ -122,7 +46,7 @@ size_t accumulate_response(char *ptr, size_t size, size_t nmemb, void *userdata)
 }
 
 void
-generate_token(Curl &curl, std::string &token)
+generate_token(mycurlpp::Curl &curl, std::string &token)
 {
 	curl.set_path("/api/v1/new");
 	curl.setopt(CURLOPT_POSTFIELDSIZE, 0L);
@@ -151,7 +75,7 @@ generate_token(Curl &curl, std::string &token)
 	}
 }
 
-std::string get_auth_token(Curl &curl)
+std::string get_auth_token(mycurlpp::Curl &curl)
 {
 	static std::string token;
 	if (0 == token.length()) {
@@ -167,7 +91,7 @@ std::string get_auth_token(Curl &curl)
 }
 
 void
-manipulate_panels(Curl &curl)
+manipulate_panels(mycurlpp::Curl &curl)
 {
 	std::ostringstream path;
 	std::string token = get_auth_token(curl);
@@ -235,7 +159,7 @@ static void resolve_callback(
                     "\tmulticast: " << !!(flags & AVAHI_LOOKUP_RESULT_MULTICAST) << std::endl <<
                     "\tcached: " << !!(flags & AVAHI_LOOKUP_RESULT_CACHED) << std::endl
 			;
-            Curl curl(host_name, port);
+            mycurlpp::Curl curl(host_name, port);
             manipulate_panels(curl);
             avahi_free(t);
         }
